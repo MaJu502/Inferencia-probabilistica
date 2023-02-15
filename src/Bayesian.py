7e0567347a97709f5534677b19cd0174ef80d77a
MarcoJurado20308 <60157453+MaJu502@users.noreply.github.com>
2023-02-15 02:13:12 -0600

    Update Bayesian.py

--- src/Bayesian.py
+++ src/Bayesian.py
@@ -6,5 +6,18 @@
 from Nodes import *
 
 class Bayes:
-    def __init__(self) -> None:
-        pass
+    def __init__(self,nodosBayes) -> None:
+        self.nodosBayes = nodosBayes
+
+    # forma compacta de red baysiana
+    def compacta(self):
+        compactaSTR = "\n".join(str(nodoBayesiano) for nodoBayesiano in self.nodosBayes) + "\n" #.join para iterar los elementos.
+        return compactaSTR
+
+    # factores de la red - needs work
+    def factoresRed(self):
+        factorSRT = "\n".join(str(tabla) for tabla in self.nodosBayes) + "\n" #.join para iterar los elementos.
+        return factorSRT
+
+    # esta descrita la red?
+    #def descrita(self):
